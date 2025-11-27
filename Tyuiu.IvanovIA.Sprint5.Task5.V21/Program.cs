360e7d34c17e4fa6faec4b151771909962e443c5
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-11-27 22:31:38 +0300

    Task5

---
+++ Tyuiu.IvanovIA.Sprint5.Task5.V21/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.IvanovIA.Sprint5.Task5.V21.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Сиволап Е.М. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #12                                                             *");
+        Console.WriteLine("* Выполнил: Сиволап Егор Максимович | ИИПб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая заменит все строчные русские буквы на       *");
+        Console.WriteLine("* заглавные.                                                              *");
+        Console.WriteLine("* Полученный результат сохранит в файл OutPutDataFileTask7V17.txt.        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        string path = @"C:\DataSprint5\InPutDataFileTask5V21.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+    }
+}
