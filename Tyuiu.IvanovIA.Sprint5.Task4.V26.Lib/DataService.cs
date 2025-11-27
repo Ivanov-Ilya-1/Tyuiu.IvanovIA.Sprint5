9790c1e6e9c624df967cf71ce50a9a607a258413
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-11-27 21:19:48 +0300

    Task4

---
+++ Tyuiu.IvanovIA.Sprint5.Task4.V26.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.IvanovIA.Sprint5.Task4.V26.Lib
+{
+    public class DataService : ISprint5Task4V26
+    {
+        public double LoadFromDataFile(string path)
+        {
+            NumberFormatInfo info = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = "."
+            };
+            string strX = File.ReadAllText(path);
+            double x = double.Parse(strX, info);
+            double res = (Math.Pow(x,3) + Math.Sin(x)) * Math.Cos(x);
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.IvanovIA.Sprint5.Task4.V26/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.IvanovIA.Sprint5.Task4.V26.Lib;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #5 | Выполнил: Иванов И.А. | ИИПб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #18                                                             *");
+        Console.WriteLine("* Выполнил: Иванов Илья Анатольевич | ИИПб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет выражение и сохраняет результат   *");
+        Console.WriteLine("* в текстовый файл                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        string path = @"C:\DataSprint5\InPutDataFileTask4V26.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+    }
+}
