59a136a6fb94d9c3f0650e13dc0a387e0ff3db03
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-11-27 22:39:58 +0300

    Task6

---
+++ Tyuiu.IvanovIA.Sprint5.Task6.V4.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.IvanovIA.Sprint5.Task6.V4.Lib
+{
+    public class DataService : ISprint5Task6V4
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string str = File.ReadAllText(path);
+
+            MatchCollection matches = Regex.Matches(str, "[А-ЯЁ]");
+
+            return matches.Count;
+        }
+    }
+}
---
+++ Tyuiu.IvanovIA.Sprint5.Task6.V4.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyuiu.IvanovIA.Sprint5.Task6.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 1;
+            Assert.AreEqual(1, x);
+        }
+    }
+}
