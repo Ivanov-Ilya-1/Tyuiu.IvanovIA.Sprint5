2824eade6b736217e365920eee62c118dd5cd3dd
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-11-27 22:02:27 +0300

    Task7

---
+++ Tyuiu.IvanovIA.Sprint5.Task7.V17.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.IvanovIA.Sprint5.Task7.V17.Lib
+{
+    public class DataService : ISprint5Task7V17
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = @"C:\DataSprint5\OutPutDataFileTask7V17.txt";
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            if (fileInfo.Exists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string content = File.ReadAllText(path);
+            string processedContent = Regex.Replace(content, "нн", "н");
+            File.WriteAllText(pathSaveFile, processedContent);
+
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.IvanovIA.Sprint5.Task7.V17.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.IvanovIA.Sprint5.Task7.V17.Lib;
+namespace Tyuiu.IvanovIA.Sprint5.Task7.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 1;
+            Assert.IsTrue(x == 1);
+        }
+    }
+}
