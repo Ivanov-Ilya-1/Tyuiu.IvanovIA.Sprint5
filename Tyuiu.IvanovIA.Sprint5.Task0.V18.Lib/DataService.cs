435f271b5868048adbdde5d658963b6c3f68de42
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-11-27 20:48:22 +0300

    Add project files.

---
+++ Tyuiu.IvanovIA.Sprint5.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.IvanovIA.Sprint5.Task0.V18.Lib
+{
+    public class DataService : ISprint5Task0V18
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutputFileTask0.txt" });
+            double y = x / (Math.Sqrt(Math.Pow(x,2) + x));
+            y = Math.Round(y, 3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.IvanovIA.Sprint5.Task0.V18.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.IvanovIA.Sprint5.Task0.V18.Lib;
+namespace Tyuiu.IvanovIA.Sprint5.Task0.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 1;
+            Assert.IsNotNull(x);
+        }
+    }
+}
